f98281a61e4c19f6ca2bc832276fca7319f96e27
ML-std <ML-std@github. com>
2022-09-06 14:06:00 +0300

    App js changed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> hi there.
+          Edit <code></code> hi there.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hi LOL or Well Wellss
+          Hi And Welcome to everybody
         </a>
       </header>
     </div>
